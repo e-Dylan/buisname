25eb423bbf552fba0510ae1d9b7ec4b7ce787875
e-Dylan <dylan@alignsoft.ca>
2020-12-21 15:29:26 -0500

    adv section

--- client/src/components/AdvertisingSection.tsx
+++ client/src/components/AdvertisingSection.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import '../App.css';
 
+import './styles/AdvertisingSection.scss';
+
+// Images/Icons
+import launchImage from '../resources/advertising-section/launch-image.svg';
+
 const AdvertisingSection = () => {
 	return (
 		<div className="container-center">
 			<div className="section-title">
 				<div className="section-title-text">THE PRODUCT</div>
 				<div className="section-title-underline"></div>
+			</div>
+
+			<div className="advertising-container">
+				<div className="image-col">
+					<img className="launch-image" src={launchImage} />
+				</div>
+				<div className="text-col">
+					<span className="title-text">Discover available domains by</span>
+				</div>
 			</div>
 		</div>
 	);
